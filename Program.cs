dc5bbfc8c098944a6534c39301ff5426f1682c53
Alex Shaburov <frosty174@yandex.ru>
2023-06-13 23:36:43 +0500

    added solutions of task 50

--- Program.cs
+++ Program.cs
@@ -52,29 +52,32 @@
 
 // Решение: 
 
-Console.Write("Введите строку: ");
-int row = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Введите столбец: ");
-int columns = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 5;
-int m = 7;
-Random random = new Random();
-int[,] arr = new int[n, m];
-Console.WriteLine("Исходный массив: ");
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        arr[i, j] = random.Next(10, 99);
-        Console.Write("{0} ", arr[i, j]);
-    }
-    Console.WriteLine();
-}
-if (row < 0 | row > arr.GetLength(0) - 1 | columns < 0 | columns > arr.GetLength(1) - 1)
-{
-    Console.WriteLine("Элемент не существует  ");
-}
-else
-{
-    Console.WriteLine("Значение элемента массива = {0}", arr[row, columns]);
-}
+// Console.Write("Введите строку: ");
+// int row = Convert.ToInt32(Console.ReadLine()) - 1;
+// Console.Write("Введите столбец: ");
+// int columns = Convert.ToInt32(Console.ReadLine()) - 1;
+// int n = 5;
+// int m = 7;
+// Random random = new Random();
+// int[,] arr = new int[n, m];
+// Console.WriteLine("Исходный массив: ");
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = random.Next(10, 99);
+//         Console.Write("{0} ", arr[i, j]);
+//     }
+//     Console.WriteLine();
+// }
+// if (row < 0 | row > arr.GetLength(0) - 1 | columns < 0 | columns > arr.GetLength(1) - 1)
+// {
+//     Console.WriteLine("Элемент не существует  ");
+// }
+// else
+// {
+//     Console.WriteLine("Значение элемента массива = {0}", arr[row, columns]);
+// }
+
+//=====================================
+
